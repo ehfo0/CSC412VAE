5a47483fa6e5a13ed8193f9518b978445889e1b9
Matt Craddock <matt.craddock@mail.utoronto.ca>
2016-04-27 17:50:33 -0400

    Added 9

--- vae_vis.py
+++ vae_vis.py
@@ -55,7 +55,6 @@ for dim in [2,50]:
                 plt.title("50D Reconstruction",fontsize=font)
     vae.sess.close()
 plt.subplots_adjust(wspace=0,hspace=0)
-#plt.tight_layout()
 plt.savefig('plots/img/vis_recon.png',dpi=300)
 for dim in [2,50]:
     vae=vae_save.load_and_run(dim,1.0,0,0,1,trial_num=0,nan=299)
@@ -64,7 +63,6 @@ for dim in [2,50]:
     x_sample2, y_sample2 = mnist.test.next_batch(5000)
     z_mu = vae.transform(x_sample2)
     fig, ax = plt.subplots(1,1, figsize=(8,6))
-    #plt.figure(figsize=(8, 6))
     # define the colormap
     cmap = plt.cm.jet
     # extract all colors from the .jet map
@@ -73,20 +71,14 @@ for dim in [2,50]:
     cmap = cmap.from_list('Custom cmap', cmaplist, cmap.N)
     # define the bins and normalize
     ticks = np.linspace(0.5,9.5,10)
-    bounds = np.linspace(0,9,10)
+    bounds = np.linspace(0,10,11)
     norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
     # make the scatter
     scat = ax.scatter(z_mu[:, 1], z_mu[:, 0],c=np.argmax(y_sample2, 1),cmap=cmap, norm=norm)
     # create a second axes for the colorbar
     ax2 = fig.add_axes([0.95, 0.1, 0.03, 0.8])
     cb = mpl.colorbar.ColorbarBase(ax2, cmap=cmap, norm=norm, spacing='proportional', ticks=ticks, boundaries=bounds, format='%1i')
-    # scat=plt.scatter(z_mu[:, 1], z_mu[:, 0], c=np.argmax(y_sample2, 1))
-    # scat.axes.get_xaxis().set_visible(False)
-    # scat.axes.get_yaxis().set_visible(False)
-    # plt.colorbar()
-    #plt.tight_layout()
     plt.savefig('plots/img/vis_scatter%s.png' % dim,dpi=300)
-    exit(0)
     if dim==2:
         lattice(mean_dims,[0,1],1,2)
     elif dim==50:
